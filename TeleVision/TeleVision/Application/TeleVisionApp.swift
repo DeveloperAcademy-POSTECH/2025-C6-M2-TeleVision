ef8f848f67edd2dbd8ce518eedb174dada2a34c6
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2025-10-13 02:34:56 +0900

    init: 프로젝트 초기 세팅

---
+++ TeleVision/TeleVision/Application/TeleVisionApp.swift
@@ -0,0 +1,32 @@
+//
+//  TeleVisionApp.swift
+//  TeleVision
+//
+//  Created by 김현기 on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TeleVisionApp: App {
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ TeleVision/TeleVisionTests/TeleVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeleVisionTests.swift
+//  TeleVisionTests
+//
+//  Created by 김현기 on 10/13/25.
+//
+
+import Testing
+@testable import TeleVision
+
+struct TeleVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
