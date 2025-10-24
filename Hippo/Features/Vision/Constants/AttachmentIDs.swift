f7511f4c6eb277297c608a77fd5188a09bca383e
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2025-10-25 03:22:37 +0900

    feat: ImmersiveSurgeryView Lo-Fi 구현
    - 상단 토글 버튼 Lo-Fi
    - 하단 메뉴바 Lo-Fi
    - 하단 좌우 버튼 클릭시 화면UI 구현해야함

---
+++ Hippo/Features/Vision/Constants/AttachmentIDs.swift
@@ -0,0 +1,11 @@
+//
+//  AttachmentIDs.swift
+//  Hippo
+//
+//  Created by 김현기 on 10/24/25.
+//
+
+enum AttachmentIDs {
+    static let topToggleButton = "TopToggleButton"
+    static let bottomMenuBar = "BottomMenuBar"
+}
---
+++ Hippo/Features/Vision/UI/Immersive/SurgeryViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SurgeryViewModel.swift
+//  HippoVision
+//
+//  Created by 김현기 on 10/25/25.
+//
+
+import SwiftUI
+
+@MainActor
+@Observable
+public class SurgeryViewModel {
+    public var isMenuActive: Bool = true
+    public var isEndoscopicActive: Bool = false
+}
