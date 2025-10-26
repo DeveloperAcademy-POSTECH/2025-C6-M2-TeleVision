7a1d5af4e4fd20c9a3777808e35edc4becc432b7
Hidy <roiyeon@gmail.com>
2025-10-26 15:20:22 +0900

    feat: #27 3D 모델 추가를 위한 EntityPlacementService 등록
    - attach / detach 함수 구현
    - entityID 로 entity 로드 -> gesture 부착 후 anchor에 붙임

---
+++ Hippo/Features/Vision/Runtime/Service/EntityPlacementService.swift
@@ -0,0 +1,63 @@
+//
+//  EntityPlacementService.swift
+//  Hippo
+//
+//  Created by yunsly on 10/26/25.
+//
+
+import simd
+import RealityKit
+
+public protocol AnchorServicing {
+    func attach(entityID: String, to anchor: AnchorEntity) async throws
+    func detach(entityID: String) async throws
+}
+
+struct EntityPlacementService: AnchorServicing {
+    
+    func placeAnchorInFront() -> AnchorEntity {
+        let pos = HeadPose.instance.position
+        var fwd = HeadPose.instance.forward
+        
+        let len2 = simd_length_squared(fwd)
+        fwd /= sqrt(len2) // 정규화
+        
+        let up = SIMD3<Float>(0, 1, 0)
+        let target = pos + fwd * 1.0 + up * (-0.5)
+        
+        return AnchorEntity(world: target)
+    }
+    
+    func attach(entityID: String, to anchor: AnchorEntity) async throws {
+        Task { @MainActor in
+            if let entity = try? await Entity(named: entityID) {
+                
+                // 조명 제거
+                if let lightEntity = entity.findEntity(named: "Light") {
+                    lightEntity.removeFromParent()
+                }
+                
+                // Gesture
+                let bounds = entity.visualBounds(relativeTo: nil)
+                let size   = bounds.extents
+                let box = ShapeResource.generateBox(size: size)
+                entity.components.set(CollisionComponent(shapes: [box]))
+                var manipulationComponent = ManipulationComponent()
+                manipulationComponent.releaseBehavior = .stay
+                
+                entity.components.set(
+                    [manipulationComponent, InputTargetComponent()]
+                )
+                
+                // Anchor 에 부착
+                anchor.addChild(entity)
+            }
+        }
+    }
+    
+    func detach(entityID: String) async {
+        if let entity = try? await Entity(named: entityID) {
+            entity.removeFromParent()
+        }
+    }
+}
