6a2ae0fb74761d894cea262be62142da24b96198
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2025-11-02 20:42:08 +0900

    feat: HomeView Empty상황 UI 추가

---
+++ Hippo/Features/Vision/UI/Home/Components/PatientSection/EmptyPatientView.swift
@@ -0,0 +1,14 @@
+//
+//  EmptyPatientView.swift
+//  Hippo
+//
+//  Created by 김현기 on 11/2/25.
+//
+
+import SwiftUI
+
+struct EmptyPatientView: View {
+    var body: some View {
+        
+    }
+}
