916911de00102d138df812a25c35123796b7fa03
Opunundo <hyeok3822@gmail.com>
2025-11-03 17:05:51 +0900

    feat: UI구조 수정, 상태에 따른 버튼 UI변경, HomeView 기본 버튼 구현
    UI구조 수정 내용
    RootView
        -HomeView
           -TodaysSurgeryView
           -PatientDetailView
        -CameraView
    로 구조를 수정하였습니다.
    
    상태에 따른 버튼 UI 변경 내용
    탭바 우측의 버튼 HomeView일 때와 StreamingControlView 일 때 아이콘이 달라지도록 분기처리를 하였습니다.
    
    HomeView에 존재하는 기본 버튼 구현하였습니다. HI-FI 디자인은 추후 입힐 예정입니다.

--- Hippo/Features/Mac/UI/RootView.swift
+++ Hippo/Features/Mac/UI/RootView.swift
@@ -7,22 +7,56 @@
 
 import SwiftUI
 
+enum Tabs{
+    case Home
+    case StreamingControl
+}
+
 struct RootView: View {
+    @State private var selectedTab: Tabs = .Home
+    
     var body: some View {
-        TabView {
-            Tab {
-                HomeView()
-            } label: {
-                Text("Patients")
+        NavigationStack {
+            //탭바
+            TabView(selection: $selectedTab) {
+                Tab("Patients", systemImage: "", value: .Home) {
+                    HomeView()
+                        .navigationTitle("Patient")
+                }
+                Tab("Camera", systemImage: "", value: .StreamingControl) {
+                    StreamingControlView()
+                        .navigationTitle("Camera")
+                }
             }
-            Tab {
-                StreamingControlView()
-            } label: {
-                Text("Camera")
+        }
+        .toolbar {
+            ToolbarItem {
+                //툴바 우측 버튼
+                Button {
+                    //TODO: 상황에 따라 버튼 기능 변경
+                    switch selectedTab {
+                    case .Home:
+                        //CASE 1-2: HomeView에서 환자를 선택하지 않으면 버튼 없음
+                        //CASE 1-2: HomeView에서 환자를 선택하면 수술 생성 버튼
+                        print("홈뷰기능")
+                    case .StreamingControl:
+                        //CASE 2-1: StreamingControlView에서는 우측 디버깅 창 토글 버튼
+                        print("스트리밍 뷰 기능")
+                    }
+                } label: {
+                    switch selectedTab {
+                    case .Home:
+                        //TODO: 환자 선택 전에는 비활성화/감춤 처리
+                        Label("Create", systemImage: "plus")
+                    case .StreamingControl:
+                        Label("Debug", systemImage: "sidebar.right")
+                    }
+                }
             }
         }
     }
 }
+
 
 #Preview {
     RootView()
