98daac14fdcc0b59c6d3b26721479536933240b7
Opunundo <hyeok3822@gmail.com>
2025-11-04 21:52:34 +0900

    feat: 영상 프리뷰 연결 작업 중
    TODO:
    - DevicePreview 연결 마무리 하기
    - 헤더에 크게 Input 넣고, 아래에 Input 글자들을 뺴기

--- Hippo/Features/Mac/UI/StreamingControl/StreamingControlView.swift
+++ Hippo/Features/Mac/UI/StreamingControl/StreamingControlView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 enum VideoInputMode {
     case SideBySide
@@ -35,14 +36,16 @@ struct StreamingControlView: View {
     @State private var selectedLeftDeviceId: String = "FaceTime HD Camera"
     @State private var selectedRightDeviceId: String = "UVC Capture A"
 
-    // Monocular
-    @State private var selectedMonoDeviceId: String = "FaceTime HD Camera"
+    // Monoc
+    @State private var selectedDeviceId: String = "FaceTime HD Camera"
     
-    private let cardViewMaxWidth: CGFloat = 600
+    private let videoLayer = AVSampleBufferDisplayLayer()
+    
+    private let pickerCardViewMaxWidth: CGFloat = 600
 
     var body: some View {
         VStack {
-            // Mode
+            // 비디오인풋 선택
             VStack(alignment: .leading) {
                 Text("VideoInputMode")
                 Picker("", selection: $videoInputMode) {
@@ -50,7 +53,7 @@ struct StreamingControlView: View {
                     Text("MonoVideo").tag(VideoInputMode.MonoVideo)
                 }
                 .pickerStyle(.segmented)
-                .frame(maxWidth: cardViewMaxWidth, alignment: .leading)
+                .frame(maxWidth: pickerCardViewMaxWidth, alignment: .leading)
             }
             .padding()
             .background(
@@ -58,7 +61,7 @@ struct StreamingControlView: View {
                     .fill(Color.gray.opacity(0.15))
             )
 
-            // Camera selection
+            // 카메라인풋선택: 3D로 출력하기 위한 영상 vs 2D로 출력하기 위한 영상
             VStack {
                 HStack {
                     Text("CameraInputMode")
@@ -72,47 +75,54 @@ struct StreamingControlView: View {
 
                     }
                 }
-                .frame(maxWidth: cardViewMaxWidth)
+                .frame(maxWidth: pickerCardViewMaxWidth)
 
+                
                 switch videoInputMode {
-                case .SideBySide:
-                    // 두 개의 영상 처리기에서 받아오는 경우 두 개의 피커 생성 또는 단일 입력
+                  
+                case .SideBySide:  //3D로 출력하기 위한 영상(SBS)을 가져오려는 경우
                     switch cameraInputMode {
-                    case .DualInput:
+                       
+                    case .DualInput: //SBS 영상을 두 개의 영상 처리기에서 받아오려는 경우
+                        //TODO: 피커 내부 selectedDeviceId를 실제 데이터로 교체
+                        //TODO: ForEach 내부 device는 실제 [AVCaptureDevice]로 교체
                         HStack {
                             Picker("Left Camera", selection: $selectedLeftDeviceId) {
                                 ForEach(devices, id: \.self) { device in
                                     Text(device).tag(device)
                                 }
                             }
-
                             Picker("Right Camera", selection: $selectedRightDeviceId) {
                                 ForEach(devices, id: \.self) { device in
                                     Text(device).tag(device)
                                 }
                             }
                         }
-                        .frame(maxWidth: cardViewMaxWidth)
-
-                    case .SingleInput:
+                        .frame(maxWidth: pickerCardViewMaxWidth)
+                        
+                    case .SingleInput: //SBS 영상을 한 개의 영상 처리기에서 받아오려는 경우
+                        //TODO: 피커 내부 selectedDeviceId를 실제 데이터로 교체
+                        //TODO: ForEach 내부 device는 실제 [AVCaptureDevice]로 교체
                         VStack(alignment: .leading) {
-                            Picker("Camera", selection: $selectedMonoDeviceId) {
+                            Picker("Camera", selection: $selectedDeviceId) {
                                 ForEach(devices, id: \.self) { device in
                                     Text(device).tag(device)
                                 }
                             }
-                            .frame(maxWidth: cardViewMaxWidth)
+                            .frame(maxWidth: pickerCardViewMaxWidth)
                         }
                     }
-
-                case .MonoVideo:
+                    
+                case .MonoVideo: //2D로 출력하기 위한 영상을 가져오려는 경우
                     VStack(alignment: .leading) {
-                        Picker("Camera", selection: $selectedMonoDeviceId) {
+                        //TODO: 피커 내부 selectedDeviceId를 실제 데이터로 교체
+                        //TODO: ForEach 내부 device는 실제 [AVCaptureDevice]로 교체
+                        Picker("Camera", selection: $selectedDeviceId) {
                             ForEach(devices, id: \.self) { device in
                                 Text(device).tag(device)
                             }
                         }
-                        .frame(maxWidth: cardViewMaxWidth)
+                        .frame(maxWidth: pickerCardViewMaxWidth)
                     }
                 }
                 
@@ -124,6 +134,21 @@ struct StreamingControlView: View {
             )
 
             Spacer()
+            
+            //영상 프리뷰
+            DevicePreview(preview: videoLayer)
+            //                .frame(width: 640, height: 360)
+            //                .background(Color.black.opacity(0.1))
+            
+            //TODO: 스트리밍 시작 버튼
+            Button {
+                //TODO: RTP패킷 전송 기능 연결
+            } label: {
+                HStack {
+                    Image(systemName: "play.fill") //TODO: 커스텀 아이콘으로 변경
+                    Text("Start Streaming")
+                }
+            }
         }
         .padding()
     }
