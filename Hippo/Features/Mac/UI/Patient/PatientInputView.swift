c35ef4a423c91e225f3e42bc62a0dd2e986e5862
Opunundo <hyeok3822@gmail.com>
2025-11-04 16:52:30 +0900

    fix: 오타수정
    수술 타이틀 -> 환자등록번호

--- Hippo/Features/Mac/UI/Patient/PatientInputView.swift
+++ Hippo/Features/Mac/UI/Patient/PatientInputView.swift
@@ -10,33 +10,38 @@ import SwiftUI
 struct PatientInputView: View {
     @Binding var isPatientInputSheetPresented: Bool
     @State private var birthDate = Date()
-    
+
     private var age: Int {
         let now = Date()
         let calendar = Calendar.current
-        let yearDiff = calendar.dateComponents([.year], from: birthDate, to: now).year ?? 0
+        let yearDiff =
+            calendar.dateComponents([.year], from: birthDate, to: now).year ?? 0
         return max(yearDiff, 0)
     }
-    
+
     var body: some View {
         Section(header: Text("환자 추가하기")) {
-                Form {
-                   TextField("수술 타이틀", text: .constant(""))
-                    TextField("이름", text: .constant(""))
-                    Picker("성별", selection: .constant(0)) {
-                        Text("남성")
-                        Text("여성")
-                    }
-                    .pickerStyle(.segmented)
-                    HStack {
-                        DatePicker("출생날짜", selection: $birthDate, displayedComponents: [.date])
-                            .environment(\.locale, Locale(identifier: "ko_KR"))
-                        Text("\(age)세")
-                    }
+            Form {
+                TextField("환자등록번호", text: .constant(""))
+                TextField("이름", text: .constant(""))
+                Picker("성별", selection: .constant(0)) {
+                    Text("남성")
+                    Text("여성")
+                }
+                .pickerStyle(.segmented)
+                HStack {
+                    DatePicker(
+                        "출생날짜",
+                        selection: $birthDate,
+                        displayedComponents: [.date]
+                    )
+                    .environment(\.locale, Locale(identifier: "ko_KR"))
+                    Text("\(age)세")
+                }
             }
         }
         .padding()
-        
+
         HStack {
             Button("취소") {
                 isPatientInputSheetPresented = false
@@ -53,4 +58,3 @@ struct PatientInputView: View {
 #Preview {
     RootView()
 }
-
