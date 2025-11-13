2933688d30e88555519b598a1a3465f07a4819ea
Opunundo <hyeok3822@gmail.com>
2025-11-13 21:53:00 +0900

    feature: HomeView HiFi 환자추가 버튼 제외 작업 완료
    중간 저장

--- Hippo/Features/Mac/UI/Root/RootView.swift
+++ Hippo/Features/Mac/UI/Root/RootView.swift
@@ -24,17 +24,12 @@ struct RootView: View {
                     HomeView()
                 case .StreamingControl:
                     StreamingControlView()
-                        .navigationTitle(Text("Hippo"))  //툴바 영역 허전해서 하나 넣어드림
+                        .navigationTitle("Hippo")
                 }
             }
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {  //.primaryAction: 툴바 아이템을 우측정렬
-                    // 상단 탭 전환용 세그먼트 컨트롤
-                    Picker("Section", selection: $selectedTab) {
-                        Text("Patients").tag(Tabs.Home)
-                        Text("Camera").tag(Tabs.StreamingControl)
-                    }
-                    .pickerStyle(.segmented)
+                    TabPicker(selectedTab: $selectedTab)
                 }
             }
         }
