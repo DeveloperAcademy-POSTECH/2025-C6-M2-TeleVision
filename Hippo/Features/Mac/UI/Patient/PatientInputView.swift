2fe6c87ab672013739fe569cf2404a3f117caa43
Opunundo <hyeok3822@gmail.com>
2025-11-14 11:51:22 +0900

    feature: PatientInputView 색상 입히기 완료
    picker는 tint로 처리했는데 텍스트 색상이 디자인과 안 맞음. picker는 텍스트 색상 커스텀 불가.
    그러면 버튼 커스텀인데 피커 같은 형태가 잘 안 나와서 일단 피커에 틴트로 둠

--- Hippo/Features/Mac/UI/Patient/PatientInputView.swift
+++ Hippo/Features/Mac/UI/Patient/PatientInputView.swift
@@ -16,49 +16,103 @@ struct PatientInputView: View {
     let onSave: () async -> Void
 
     var body: some View {
-        Section(header: Text("Add Patient")) {
-            Form {
-                TextField("Patient Number", text: $state.patientNumber)
-                TextField("Name", text: $state.name)
-                Picker("Gender", selection: $state.selectedGender) {
-                    Text("Male").tag(Gender.male)
-                    Text("Female").tag(Gender.female)
-                }
-                .pickerStyle(.segmented)
-                HStack {
-                    DatePicker(
-                        "Birth Date",
-                        selection: $state.birthDate,
-                        displayedComponents: [.date]
-                    )
-                    .environment(\.locale, Locale(identifier: "ko_KR"))
-                    Text("Age \(state.age)")
-                }
-            }
-        }
-        .padding()
-        
-        Divider()
-            .padding(.horizontal)
-        
-        HStack {
-            Button("Cancel") {
-                isPresentingPatientInput = false
-            }
-            Button("Save") {
-                if state.isValid {
-                    Task {
-                        await onSave()
+        VStack {
+            Section {
+                VStack {
+                    HStack(alignment: .center) {
+                        VStack(alignment: .leading) {
+                            Text("Patient Number")
+                            Spacer()
+                            
+                            Text("Name")
+                            Spacer()
+                            
+                            Text("Gender")
+                            Spacer()
+                            
+                            Text("Birth Date")
+                        }
+                        
+                        Spacer()
+                        
+                        VStack(alignment: .leading) {
+                            TextField("", text: $state.patientNumber)
+                            Spacer()
+                            
+                            TextField("", text: $state.name)
+                            Spacer()
+                            
+                            Picker("", selection: $state.selectedGender) {
+                                Text("Male").tag(Gender.male)
+                                Text("Female").tag(Gender.female)
+                            }
+                            .pickerStyle(.segmented)
+                            .tint(.hippoPrimary)
+                            Spacer()
+                            
+                            HStack {
+                                DatePicker(
+                                    "",
+                                    selection: $state.birthDate,
+                                    displayedComponents: [.date]
+                                )
+                                .environment(\.locale, Locale(identifier: "ko_KR"))
+                                Text("Age \(state.age)")
+                                    .font(.footnote)
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.hippoGray500)
+                            }
+                        }
                     }
+                    //                        HStack {
+                    //                            Text("Patient Number")
+                    //                            TextField("", text: $state.patientNumber)
+                    //                        }
+                    //                        .padding(.vertical, 4)
+                    //                        HStack {
+                    //                            Text("Patient Number")
+                    //                            TextField(" ", text: $state.name)
+                    //                        }
+                    //                        .padding(.vertical, 4)
+                    //                        Picker("Gender", selection: $state.selectedGender) {
+                    //                            Text("Male").tag(Gender.male)
+                    //                            Text("Female").tag(Gender.female)
+                    //                        }
+                    //                        .pickerStyle(.segmented)
+                    //                        .padding(.vertical, 4)
+                    //                        HStack {
+                    //                            DatePicker(
+                    //                                "Birth Date",
+                    //                                selection: $state.birthDate,
+                    //                                displayedComponents: [.date]
+                    //                            )
+                    //                            .environment(\.locale, Locale(identifier: "ko_KR"))
+                    //                            Text("Age \(state.age)")
+                    //                        }
                 }
-                isPresentingPatientInput = false
+                .font(.callout)
+                .fontWeight(.semibold)
+                .foregroundColor(.hippoGray900)
+            } header: {
+                Text("Add Patient")
+                    .font(.headline)
+                    .foregroundColor(.hippoGray500)
+                    .frame(maxWidth: .infinity, alignment: .leading)
             }
+
+            Divider()
+
+            FormActionBar(
+                isPresenting: $isPresentingPatientInput,
+                canSave: state.isValid,
+                onSave: onSave
+            )
         }
         .padding()
-
     }
 }
 
 #Preview {
     RootView()
 }
+
